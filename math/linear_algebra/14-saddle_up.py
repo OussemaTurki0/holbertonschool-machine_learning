6249ee039c2941cacbebc3726934672ec7da0333
oussematurki0 <oussematurki0@gmail.com>
2024-12-07 14:58:45 +0100

    commit

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A function that performs matrix multiplication
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication between two numpy.ndarrays.
+
+    Returns:
+    - A new numpy.ndarray resulting from the matrix multipli
