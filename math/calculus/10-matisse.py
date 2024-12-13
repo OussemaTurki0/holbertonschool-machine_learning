b6bf10eb757f2f581966513a745e6967799b9464
oussematurki0 <oussematurki0@gmail.com>
2024-12-13 11:36:10 +0100

    commit

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-
 """
 a function that calculates the derivative of a polynomial
 """
 
-def poly_derivative(poly):
 
+def poly_derivative(poly):
     """
     Args:
     poly (list of int or float): Coefficients of the polynomial, where the
@@ -16,11 +15,15 @@ def poly_derivative(poly):
             not isinstance(coef, (int, float)) for coef in poly
             ):
         return None
+
     if len(poly) == 0:
         return None
     if len(poly) < 2:
         return [0]
+
     derivative = [i * poly[i] for i in range(1, len(poly))]
+
     if not derivative:
         return [0]
+
     return derivative
