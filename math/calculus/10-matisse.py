76858188851157db4222fa3f667bc07ddc968c53
oussematurki0 <oussematurki0@gmail.com>
2024-12-13 11:32:44 +0100

    m

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 a function that calculates the derivative of a polynomial
 """
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 Calculate the sum of squares from 1 to n using the formula:
     Sum = n(n+1)(2n+1)/6
