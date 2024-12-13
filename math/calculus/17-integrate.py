b3645a4887be8280f460a3dba06ba6dc5cf86fdc
oussematurki0 <oussematurki0@gmail.com>
2024-12-13 11:34:14 +0100

    commit

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-
 """
 integration functions
 """
-def poly_integral(poly, C=0):
 
+
+def poly_integral(poly, C=0):
     """
-    Check validity of poly and C, and handle empty polyy
+    Check validity of poly and C, and handle empty poly
     """
     if (not isinstance(poly, list) or
             not isinstance(C, (int, float)) or
@@ -14,11 +14,15 @@ def poly_integral(poly, C=0):
         return None
     if not all(isinstance(x, (int, float)) for x in poly):
         return None
+
     integral = [C]
     for i in range(len(poly)):
         integral.append(poly[i] / (i + 1))
+
     integral = [int(x) if isinstance(x, float) and x.is_integer()
                 else x for x in integral]
+
     while len(integral) > 1 and integral[-1] == 0:
         integral.pop()
+
     return integral
