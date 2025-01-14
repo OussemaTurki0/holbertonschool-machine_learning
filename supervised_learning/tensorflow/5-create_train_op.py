7d37c389c479c5ccc855fba959effb57b30066a4
oussematurki0 <oussematurki0@gmail.com>
2025-01-14 22:01:40 +0100

    commit

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python3
+"""
+Creates the training operation for the neural network
+"""
+
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+
+
 def create_train_op(loss, alpha):
     """
-    Creates the training operation for the network.
-
-    Args:
-        loss (tf.Tensor): Loss of the network's prediction.
-        alpha (float): Learning rate.
-
-    Returns:
-        tf.Operation: Training operation.
+    Creates the training operation for the neural network.
     """
-    train_op = tf.train.GradientDescentOptimizer(alpha).minimize(loss)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=alpha)
+    train_op = optimizer.minimize(loss)
     return train_op
--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -1,53 +1,39 @@
-calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-calculate_loss = __import__('4-calculate_loss').calculate_loss
-create_placeholders = __import__('0-create_placeholders').create_placeholders
-create_train_op = __import__('5-create_train_op').create_train_op
-forward_prop = __import__('2-forward_prop').forward_prop
+#!/usr/bin/env python3
+"""
+Evaluates the output of a neural network
+"""
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+
+
+def evaluate(X, Y, save_path):
     """
-    Builds, trains, and saves a neural network classifier.
-
+    Evaluates the output of a neural network
     Args:
-        X_train (np.ndarray): Training input data.
-        Y_train (np.ndarray): Training labels.
-        X_valid (np.ndarray): Validation input data.
-        Y_valid (np.ndarray): Validation labels.
-        layer_sizes (list): List of nodes in each layer.
-        activations (list): Activation functions for each layer.
-        alpha (float): Learning rate.
-        iterations (int): Number of training iterations.
-        save_path (str): Path to save the model.
-
+        X (np.ndarray): The input data to evaluate
+        Y (np.ndarray): The one-hot labels for X
+        save_path (str): The location to load the model from
     Returns:
-        str: Path where the model was saved.
+        np.ndarray: The network's prediction
+        float: The accuracy of the model
+        float: The loss of the model
     """
-    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    y_pred = forward_prop(x, layer_sizes, activations)
-    loss = calculate_loss(y, y_pred)
-    accuracy = calculate_accuracy(y, y_pred)
-    train_op = create_train_op(loss, alpha)
-    
-    # Add tensors to collections
-    tf.add_to_collection("x", x)
-    tf.add_to_collection("y", y)
-    tf.add_to_collection("y_pred", y_pred)
-    tf.add_to_collection("loss", loss)
-    tf.add_to_collection("accuracy", accuracy)
-    tf.add_to_collection("train_op", train_op)
-
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for i in range(iterations + 1):
-            _, train_loss, train_accuracy = sess.run([train_op, loss, accuracy], feed_dict={x: X_train, y: Y_train})
-            valid_loss, valid_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
+        # Load the saved model
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(sess, save_path)
 
-            if i % 100 == 0 or i == iterations:
-                print(f"After {i} iterations:")
-                print(f"\tTraining Cost: {train_loss}")
-                print(f"\tTraining Accuracy: {train_accuracy}")
-                print(f"\tValidation Cost: {valid_loss}")
-                print(f"\tValidation Accuracy: {valid_accuracy}")
-        
-        saver = tf.train.Saver()
-        return saver.save(sess, save_path)
+        # Retrieve the necessary tensors from the graph
+        graph = tf.get_default_graph()
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        loss = tf.get_collection('loss')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+
+        # Evaluate the model on the given data
+        prediction, accuracy_value, loss_value = sess.run(
+            [y_pred, accuracy, loss], feed_dict={x: X, y: Y})
+
+    return prediction, accuracy_value, loss_value
