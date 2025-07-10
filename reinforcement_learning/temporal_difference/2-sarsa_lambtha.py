10e7060a9076a4b688c4ac5afda38180542b831c
oussematurki0 <oussematurki0@gmail.com>
2025-07-10 22:20:41 +0200

    commit

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -1,45 +1,66 @@
 #!/usr/bin/env python3
 """
-TD lambtha algorithm
+SARSA(λ) algorithm (with eligibility traces)
 """
+
 import numpy as np
 
 
-def td_lambtha(env, V, policy, lambtha=0.9, episodes=5000, max_steps=100,
-               alpha=0.1, gamma=0.99):
+def epsilon_greedy(Q, state, epsilon):
     """
-    Performs the TD(λ) algorithm for estimating the value function.
+    Determines the next action using the epsilon-greedy policy.
     """
+    if np.random.uniform(0, 1) > epsilon:
+        # Exploit: choose the action with the highest Q-value
+        return np.argmax(Q[state, :])
+    else:
+        # Explore: choose a random action
+        return np.random.randint(0, Q.shape[1])
+
+
+def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
+                  gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
+    """
+    Performs the SARSA(λ) algorithm (with eligibility traces) to estimate
+    a Q-table.
+    """
+    initial_epsilon = epsilon
+
     for episode in range(episodes):
-        # reset the environment and get initial state
+        # Reset and choose first action
         state = env.reset()[0]
+        action = epsilon_greedy(Q, state, epsilon)
 
         # Init. eligibility traces to zero, for all states
-        eligibility_traces = np.zeros_like(V)
+        eligibility_traces = np.zeros_like(Q)
 
-        for step in range(max_steps):
-            # Select action based on policy
-            action = policy(state)
+        for steps in range(max_steps):
+            # Take the action in the environment
+            new_state, reward, terminated, truncated, _ = env.step(action)
 
-            # Take action
-            next_state, reward, terminated, truncated, _ = env.step(action)
+            # Choose next action based on epsilon-greedy policy
+            new_action = epsilon_greedy(Q, new_state, epsilon)
 
             # TD Error (δ): reward + gamma * V(next_state) - V(state)
-            delta = reward + (gamma * V[next_state] - V[state])
+            delta = (reward + (gamma * Q[new_state, new_action]) -
+                     Q[state, action])
 
-            # Update eligibility trace for the current state
-            eligibility_traces[state] += 1
+            # Update eligibility traces, apply lambtha decay
+            eligibility_traces[state, action] += 1
+            eligibility_traces *= lambtha * gamma
 
-            # Update each state's value and eligibility trace
-            V += alpha * delta * eligibility_traces
+            # Update the Q-table
+            Q += alpha * delta * eligibility_traces
 
-            # Apply lambtha decay to eligibility traces
-            eligibility_traces *= gamma * lambtha
-
-            # Move to the next state
-            state = next_state
+            # Update to the next state & action
+            state = new_state
+            action = new_action
 
             if terminated or truncated:
                 break
 
-    return V
+        # Exploration rate decay
+        epsilon = (min_epsilon + (initial_epsilon - min_epsilon) *
+                   np.exp(-epsilon_decay * episode))
+
+    return Q
