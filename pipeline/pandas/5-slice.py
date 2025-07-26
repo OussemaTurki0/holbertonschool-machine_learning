72dce0da144ee852065ad46e2fb6f4fcae828c28
oussematurki0 <oussematurki0@gmail.com>
2025-07-26 11:56:42 +0200

    commit

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-"""Slices a DataFrame every 60 rows"""
-
-import pandas as pd
+"""
+Module to slice specific columns and rows from a pandas DataFrame.
+"""
 
 
 def slice(df):
-    """Selects every 60th row from specific columns"""
-    return df[["High", "Low", "Close", "Volume_(BTC)"]].iloc[::60]
+    """
+    Extracts specific columns and selects every 60th row from a DataFrame.
+    """
+    # Extract the specified columns
+    sliced_df = df[["High", "Low", "Close", "Volume_(BTC)"]]
+    # Select every 60th row
+    return sliced_df.iloc[::60]
